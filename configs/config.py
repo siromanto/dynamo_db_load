a785c395e9eaf45aa501ed61f3df68b8811a3621
Serhii Dmytrenko <dmytrenko.serhii@gmail.com>
2019-09-09 15:50:08 +0300

    refactor transform

---
+++ configs/config.py
@@ -0,0 +1,6 @@
+import os
+from definitions import ROOT_DIR
+
+ORIGIN_FILE = os.path.join(ROOT_DIR, 'data/diabetic_data.csv')
+TRANSFORMED_FILE = os.path.join(ROOT_DIR, 'data/transformed_data.csv')
+
--- etl/transform.py
+++ etl/transform.py
@@ -1,36 +1,41 @@
 import pandas as pd
 import numpy as np
 
-def transform_csv():
-    """Get the original csv file and make new one for dynamo db"""
-    with open('../data/diabetic_data.csv', 'r', encoding='utf-8') as f:
+from configs.config import ORIGIN_FILE, TRANSFORMED_FILE
+
+
+def transform_csv(origin_file, transformed_file, rows=300, repeat=1):
+    """
+        Get the original csv file and make new one for dynamo db
+    """
+
+    with open(origin_file, 'r', encoding='utf-8') as f:
         df = pd.read_csv(f)
-
-        print('Test')
         table_columns = df.columns.tolist()
-        print(len(table_columns))
         table_columns.insert(0, 'generated_date')
-        print(table_columns)
-        print(len(table_columns))
 
-        df1 = df.head(300)  # TODO: There is 65k records in csv. What count will we need?
+        print(f'Origin file, ROWS, COLUMNS: {df.shape}')
+        print(f'AVAILABLE COLUMNS: {table_columns}')
+
+        df1 = df.head(rows)
 
         for index, row in df1.iterrows():
             df1.loc[index, 'generated_date'] = generate_date()
 
-        print(df1)
+        df_repeated = pd.concat([df1] * repeat, ignore_index=True)
+        print(f'Transformed file, ROWS, COLUMNS: {df_repeated.shape}')
 
-        df_repeated = pd.concat([df1] * 10, ignore_index=True)
-        print(df_repeated)
+        try:
+            df_repeated.to_csv(transformed_file, index=False)
+            print('file successfully created')
+        except Exception as e:
+            print(f'Failed to create a file: {e}')
 
-        # df_repeated.to_csv('../data/transformed_data.csv', sep='\t', encoding='utf-8')
-        df_repeated.to_csv('../data/transformed_data.csv', index=False)
 
 def generate_date():
     date = f'2019-08-{np.random.randint(1, 6)}'
-    # print(date)
     return date
 
 
 if __name__ == '__main__':
-    transform_csv()
+    transform_csv(ORIGIN_FILE, TRANSFORMED_FILE, rows=300, repeat=10)
